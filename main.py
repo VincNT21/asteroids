a96f1ca34dc453579ce0b6d406ee76c01f44427c
VincNT21 <vincnt21@pm.me>
2025-01-09 09:05:22 +0100

    add player controls, add groups

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import * 
-from player import *
+from player import Player
 
 
 def main():
@@ -11,15 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        for item in updatable:
+            item.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
