c19c7ca380386614d8a7bbd5489799ac28c3ecc6
VincNT21 <vincnt21@pm.me>
2025-01-09 09:52:36 +0100

    add collisions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import * 
 from player import Player
 from asteroid import Asteroid
 from AsteroidField import *
+from circleshape import CircleShape
 
 
 def main():
@@ -10,15 +12,19 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
 
@@ -28,12 +34,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        for item in updatable:
-            item.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in asteroids:
+            if obj.check_collision(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
-        for item in drawable:
-            item.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
